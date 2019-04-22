3cdac4e334ab549287afd134a9e8e9cf19c76ca3
Satya Vamsi Moganti <vamsee1993@gmail.com>
2019-04-22 16:40:50 -0500

    update server and servers components

---
+++ init-angular-app/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: [`
+    h3 {
+      color: violet;
+    }
+`]
+})
+export class AppComponent {
+  title = 'init-angular-app';
+}
---
+++ init-angular-app/src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+//naming convention as the server class with an inside component file
+
+//decorator to enhance the classes or components
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+export class ServerComponent {
+    
+}
